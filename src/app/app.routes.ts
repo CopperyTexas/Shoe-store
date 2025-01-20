64556d59c8b2ec7481cca22f3d75d6ebd473e5c1
Denis Glebov <dengleboff@yandex.ru>
2025-01-20 16:41:13 +0500

    start

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ShopComponent } from './pages/shop/shop.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'shop', component: ShopComponent },
+  { path: 'about-us', component: AboutUsComponent },
+  { path: 'contact-us', component: ContactUsComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
---
+++ src/app/pages/shop/shop.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shop',
+  imports: [],
+  standalone: true,
+  templateUrl: './shop.component.html',
+  styleUrl: './shop.component.scss',
+})
+export class ShopComponent {}
