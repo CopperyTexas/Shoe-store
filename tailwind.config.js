d1bbf1f51eb599d7644c74feb014a50531dc249c
CopperyTexas <dengleboff@yandex.ru>
2025-01-22 22:32:27 +0500

    footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,10 @@ module.exports = {
   content: ['./src/**/*.{html,ts}'],
   theme: {
     extend: {
-      fontFamily: { sans: ['Montserrat', 'Arial', 'sans-serif'] },
+      fontFamily: {
+        sans: ['Montserrat', 'Arial', 'sans-serif'],
+        text: ['Raleway', 'Arial', 'sans-serif'],
+      },
     },
   },
   plugins: [],
