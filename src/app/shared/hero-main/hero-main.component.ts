e3a1c6b66e1f9a0812adab2b691d01883dcf3849
CopperyTexas <dengleboff@yandex.ru>
2025-01-24 23:02:58 +0500

    настроил анимацию hero

--- src/app/shared/hero-main/hero-main.component.ts
+++ src/app/shared/hero-main/hero-main.component.ts
@@ -1,15 +1,59 @@
-import { Component } from '@angular/core';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-hero-main',
   imports: [],
   templateUrl: './hero-main.component.html',
+  animations: [
+    trigger('slideInOut', [
+      state('in', style({ transform: 'translateX(0)', opacity: 1 })),
+      state('out', style({ transform: 'translateX(100%)', opacity: 0 })),
+      transition('out => in', [
+        style({ transform: 'translateX(-100%)', opacity: 0 }),
+        animate('1s ease-out'),
+      ]),
+      transition('in => out', [
+        animate(
+          '1s ease-in',
+          style({ transform: 'translateX(100%)', opacity: 0 }),
+        ),
+      ]),
+    ]),
+  ],
 })
-export class HeroMainComponent {
+export class HeroMainComponent implements OnInit, OnDestroy {
   images: string[] = [
     'assets/images/products/Product-1.png',
     'assets/images/products/Product-2.png',
     'assets/images/products/Product-3.png',
     'assets/images/products/Product-4.png',
   ];
+  currentImageIndex = 0; // Индекс текущего изображения
+  intervalId: ReturnType<typeof setInterval> | null = null; // Таймер
+  animationState = 'in';
+
+  ngOnInit(): void {
+    this.intervalId = setInterval(() => {
+      this.animationState = 'out';
+      setTimeout(() => {
+        this.currentImageIndex =
+          (this.currentImageIndex + 1) % this.images.length;
+        this.animationState = 'in';
+      }, 1000);
+    }, 5000);
+  }
+
+  ngOnDestroy(): void {
+    // Очищаем таймер при уничтожении компонента
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes)],
+  providers: [provideRouter(routes), provideAnimations()],
 });
