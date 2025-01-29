9ccf20276a6dc9f8443e9a024281c60446e7cdb7
Denis Glebov <dengleboff@yandex.ru>
2025-01-29 15:25:14 +0500

    next page

--- src/app/pages/about-us/about-us.component.ts
+++ src/app/pages/about-us/about-us.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { AchievementsComponent } from '../../shared/achievements/achievements.component';
 
 @Component({
   selector: 'app-about-us',
-  imports: [AchievementsComponent, CommonModule],
+  imports: [AchievementsComponent, CommonModule, RouterModule],
   standalone: true,
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.scss',
