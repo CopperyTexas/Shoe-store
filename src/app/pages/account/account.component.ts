90d5db692136d61ea9115357767e56e51207ecae
CopperyTexas <dengleboff@yandex.ru>
2025-04-18 00:02:30 +0500

    добавил форму аккаунта

--- src/app/pages/account/account.component.ts
+++ src/app/pages/account/account.component.ts
@@ -1,9 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-account',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './account.component.html',
   styleUrl: './account.component.scss',
 })
-export class AccountComponent {}
+export class AccountComponent implements OnInit {
+  accountForm!: FormGroup;
+  constructor(private fb: FormBuilder) {}
+  ngOnInit() {
+    this.accountForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      rememberMe: [false],
+    });
+  }
+  onSubmit() {
+    if (this.accountForm.valid) {
+      console.log(this.accountForm.value);
+    } else {
+      this.accountForm.markAllAsTouched();
+    }
+  }
+}
